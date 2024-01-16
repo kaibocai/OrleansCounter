b6ce6a0392636ade084e847ab7678fde725f5e28
kaibocai <89094811+kaibocai@users.noreply.github.com>
2024-01-16 16:47:38 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,127 @@
+using Orleans.Configuration;
+using Orleans.Runtime;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseOrleans((ctx, siloBuilder) =>
+{
+    if (ctx.HostingEnvironment.IsDevelopment())
+    {
+        siloBuilder.UseLocalhostClustering();
+        siloBuilder.AddMemoryGrainStorage("urls");
+    }
+    else
+    {
+        // aks configuration
+        //// In Kubernetes, we use environment variables and the pod manifest
+        //siloBuilder.UseKubernetesHosting();
+
+        //// Use Redis for clustering & persistence
+        //var redisConnectionString = $"{Environment.GetEnvironmentVariable("REDIS")}:6379";
+        //siloBuilder.UseRedisClustering(options => options.ConnectionString = redisConnectionString);
+        //siloBuilder.AddRedisGrainStorage("urls", options => options.ConnectionString = redisConnectionString);
+
+
+        // container app configuration
+        var connectionString = "azure storage connection string";
+        siloBuilder
+        .UseAzureStorageClustering(o => o.ConfigureTableServiceClient(connectionString))
+        .AddAzureTableGrainStorage("urls", o => o.ConfigureTableServiceClient(connectionString));
+
+        siloBuilder.Configure<ClusterOptions>(options =>
+        {
+            options.ClusterId = "urls-counter";
+            options.ServiceId = "urls";
+        });
+    }
+});
+
+var app = builder.Build();
+
+app.MapGet("/", static () => "Welcome to the URL shortener, powered by Orleans!");
+
+app.MapGet("/shorten",
+    static async (IGrainFactory grains, HttpRequest request, string url) =>
+    {
+        var host = $"{request.Scheme}://{request.Host.Value}";
+
+        // Validate the URL query string.
+        if (string.IsNullOrWhiteSpace(url) &&
+            Uri.IsWellFormedUriString(url, UriKind.Absolute) is false)
+        {
+            return Results.BadRequest($"""
+                The URL query string is required and needs to be well formed.
+                Consider, ${host}/shorten?url=https://www.microsoft.com.
+                """);
+        }
+
+        // Create a unique, short ID
+        var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+
+        // Create and persist a grain with the shortened ID and full URL
+        var shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        await shortenerGrain.SetUrl(url);
+
+        // Return the shortened URL for later use
+        var resultBuilder = new UriBuilder(host)
+        {
+            Path = $"/go/{shortenedRouteSegment}"
+        };
+
+        return Results.Ok(resultBuilder.Uri);
+    });
+
+app.MapGet("/go/{shortenedRouteSegment:required}",
+    static async (IGrainFactory grains, string shortenedRouteSegment) =>
+    {
+        // Retrieve the grain using the shortened ID and url to the original URL
+        var shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        var url = await shortenerGrain.GetUrl();
+
+        // Handles missing schemes, defaults to "http://".
+        var redirectBuilder = new UriBuilder(url);
+
+        return Results.Redirect(redirectBuilder.Uri.ToString());
+    });
+
+app.MapGet("/count",
+    static async (IGrainFactory grains, HttpRequest request) =>
+    {
+
+        // Create and persist a grain with the shortened ID and full URL
+        var counterGrain =
+            grains.GetGrain<IUrlShortenerGrain>("counter");
+
+        await counterGrain.AddCount();
+
+        return Results.Ok("Counter increased by 1");
+    });
+
+app.MapGet("/getcount",
+    static async (IGrainFactory grains, HttpRequest request) =>
+    {
+
+        // Create and persist a grain with the shortened ID and full URL
+        var counterGrain =
+            grains.GetGrain<IUrlShortenerGrain>("counter");
+        var count = await counterGrain.GetCount();
+        return Results.Ok(count.ToString());
+    });
+
+app.Run();
+
+public interface IUrlShortenerGrain : IGrainWithStringKey
+{
+    Task SetUrl(string fullUrl);
+
+    Task<string> GetUrl();
+
+    Task AddCount();
+
+    Task<int> GetCount();
+}
---
+++ UrlShortenerGrain.cs
@@ -0,0 +1,50 @@
+﻿using Orleans.Runtime;
+
+public sealed class UrlShortenerGrain(
+    [PersistentState(
+        stateName: "url",
+        storageName: "urls")]
+        IPersistentState<UrlDetails> state)
+    : Grain, IUrlShortenerGrain
+{
+    public async Task SetUrl(string fullUrl)
+    {
+        state.State = new()
+        {
+            ShortenedRouteSegment = this.GetPrimaryKeyString(),
+            FullUrl = fullUrl,
+        };
+
+        await state.WriteStateAsync();
+    }
+
+    public Task<string> GetUrl() =>
+        Task.FromResult(state.State.FullUrl);
+
+    public async Task AddCount()
+    {
+        state.State = new()
+        {
+            ShortenedRouteSegment = this.GetPrimaryKeyString(),
+            Count = state.State.Count + 1
+        };
+
+        await state.WriteStateAsync();
+    }
+
+    public Task<int> GetCount() =>
+        Task.FromResult(state.State.Count);
+}
+
+[GenerateSerializer, Alias(nameof(UrlDetails))]
+public sealed record class UrlDetails
+{
+    [Id(0)]
+    public string FullUrl { get; set; } = "";
+
+    [Id(1)]
+    public string ShortenedRouteSegment { get; set; } = "";
+
+    [Id(2)]
+    public int Count { get; set; } = 0;
+}
